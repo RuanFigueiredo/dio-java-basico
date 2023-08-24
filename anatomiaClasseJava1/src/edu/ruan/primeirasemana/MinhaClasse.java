6c8783e3917be661ccee8dc690b31c095133641b
RuanFigueiredo <ruanthiago67@gmail.com>
2023-08-24 09:53:40 -0300

    aula 1

---
+++ anatomiaClasseJava1/src/edu/ruan/primeirasemana/MinhaClasse.java
@@ -0,0 +1,10 @@
+package edu.ruan.primeirasemana;
+public class MinhaClasse {
+
+    public static void main (String[]args){
+        System.out.print("Olá, gente tudo bem?!");
+
+        
+    }
+    
+}
